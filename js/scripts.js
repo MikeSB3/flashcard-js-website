905f2fc4a2cb4e69c82c3f51e44182aca53f655a
Student <example@example.com>
2019-10-22 10:31:26 -0700

    added flashcards for functions, methods, arguments, strings, alerts, and variables.

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  $(".func").click(function(){
+    $(".func-info").fadeToggle();
+    $(".func1").fadeToggle();
+  })
+
+  $(".methods").click(function(){
+    $(".methods-info").fadeToggle();
+    $(".methods1").fadeToggle();
+  })
+
+
+  $(".arguments").click(function(){
+    $(".arguments-info").fadeToggle();
+    $(".arguments1").fadeToggle();
+  })
+
+  $(".strings").click(function(){
+    $(".strings-info").fadeToggle();
+    $(".strings1").fadeToggle();
+  })
+
+  $(".alerts").click(function(){
+    $(".alerts-info").fadeToggle();
+    $(".alerts1").fadeToggle();
+  })
+
+
+  $(".variables").click(function(){
+    $(".variables-info").fadeToggle();
+    $(".variables1").fadeToggle();
+  })
+});
