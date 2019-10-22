cd4e520c5c919da5145ed2906d6c46dd79e62a1d
Student <example@example.com>
2019-10-22 11:28:01 -0700

    add cat and dog list with buttons to call a meow and bark function.

--- js/scripts.js
+++ js/scripts.js
@@ -30,4 +30,38 @@ $(document).ready(function() {
     $(".variables-info").fadeToggle();
     $(".variables1").fadeToggle();
   })
+
+  $(".jquery").click(function(){
+    $(".jquery-info").fadeToggle();
+    $(".jquery1").fadeToggle();
+  })
+
+  $(".comments").click(function(){
+    $(".comments-info").fadeToggle();
+    $(".comments1").fadeToggle();
+  })
+
+  $(".booleans").click(function(){
+    $(".booleans-info").fadeToggle();
+    $(".booleans1").fadeToggle();
+  })
+/*
+  $(".true").click(function(){
+    $(".lastpanel").removeClass();
+    $(".lastpanel").addClass("green-background");
+  })
+
+  $(".false").click(function(){
+    $(".lastpanel").removeClass();
+    $(".lastpanel").addClass("red-background");
+  })
+*/
+
+  $(".meow").click(function() {
+    $("ul.cat-list").prepend("<li>Meow!</li>");
+  });
+
+  $(".bark").click(function() {
+    $("ul.dog-list").prepend("<li>Bark!</li>");
+  });
 });
